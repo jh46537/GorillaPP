9f987ff8f95b6a6513e0daac4ef47fe3d50e30ce
paleolithicman <768632866@qq.com>
2021-09-06 19:10:44 -0500

    primate assembler(alpha)

---
+++ assembler/npu.s
@@ -0,0 +1,8 @@
+ETHERNET, , , , ,R0, , , ,6
+IPV4, , , , ,R0, , , ,5
+LOOKUP,WEN,WEN, , ,R2,R1,R0,R3,0
+LOOKUP_POST, , , , ,R0,R3, , ,3
+UPDATE, , ,EN, ,R1,R0, , ,2
+UPDATE_POST, , , ,WEM,R1, ,R1, ,2
+EXCEPTION, , , ,WEN, , ,R0, ,0
+RETURN, , , , , , , , ,0
