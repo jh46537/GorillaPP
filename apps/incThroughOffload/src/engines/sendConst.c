f6bf56315940e712805192ec9e8526bd6fcbb675
Alex Hsu <alex.hsu@utexas.edu>
2021-10-27 13:13:03 -0500

    [apps] port inc example app
    update to chisel3
    clean up syntax

--- apps/incThroughOffload/src/engines/sendConst.c
+++ apps/incThroughOffload/src/engines/sendConst.c
@@ -1,14 +1,11 @@
-#pragma INPUT uint32_t 
-#pragma OUTPUT uint32_t 
+#pragma INPUT  uint32_t
+#pragma OUTPUT uint32_t
 
-GS_GET()
-{
- Output = 2;
- State = GS_SEND;
+GS_GET() {
+  Output = 2;
+  State = GS_SEND;
 }
 
-GS_SEND()
-{
- finish(); 
+GS_SEND() {
+  finish();
 }
-
--- apps/incThroughOffload/src/test/TopTest.scala
+++ apps/incThroughOffload/src/test/TopTest.scala
@@ -1,105 +1,99 @@
-//class TopTests(o: Top) extends Tester(o, Array(o.io)) {
-class TopTests(o: Top) extends gTester(o) {
-  defTests {
-    val inputData = List(1, 2, 4, 8)
-    val svars = new HashMap[Node, Node]()
-    val ovars = new HashMap[Node, Node]()
-    val iDelay = 2
+import chisel3.iotesters.PeekPokeTester
+import org.scalatest._
+import flatspec._
+import matchers.should._
 
-    //Spin for a while without any test input
-    for (time <- 0 until 5) {
-      svars(o.io.in.valid) = Bool(false)
-      //svars(o.io.in.bits) = UFix(0) 
-      svars(o.io.in.bits.a) = UFix(0) 
-      svars(o.io.pcIn.valid)= Bool(false)
-      svars(o.io.pcIn.bits.pcType)= Pcounters.pcReset
-      svars(o.io.pcIn.bits.moduleId)= UFix(0) 
-      svars(o.io.pcIn.bits.portId)= UFix(0) 
-      step(svars, ovars, false)
-    }
 
-    //Reset the PC Ring
-    svars(o.io.pcIn.valid) = Bool(true)
-    svars(o.io.pcIn.bits.request) = Bool(true) 
-    svars(o.io.pcIn.bits.pcType) = Pcounters.pcReset
-    step(svars, ovars)
-    svars(o.io.pcIn.valid) = Bool(false)
-    while(ovars(o.io.pcOut.valid).litValue().intValue == 0) {
-      step(svars, ovars)
-    }
-    println("PCREPORT: Performance counter reset received")
+class TopTests(c: Top) extends gTester[Top](c) {
+  val inputData = List(1, 2, 4, 8)
+  val iDelay = 2
 
-    //Actual tests 
-    var sourced = 0
-    var sourcedIndex = 0
-    var sinked = 0
-    var sinkedIndex = 0
-    var allPassed = true
-    var cycles = 0
-    val numOfInputs = 100 
-    while(sourced < numOfInputs || sinked < numOfInputs) {
-      if ((sourced < numOfInputs) && (cycles % iDelay == 0)) {
-        //svars(o.io.in.bits) = Bits(inputData(sourcedIndex))
-        svars(o.io.in.bits.a) = Bits(inputData(sourcedIndex))
-        svars(o.io.in.valid) = Bool(true)
-        svars(o.io.out.ready) = Bool(true)
-      } else {
-        //svars(o.io.in.bits) = UFix(0) 
-        svars(o.io.in.bits.a) = UFix(0) 
-        svars(o.io.in.valid) = Bool(false)
-        svars(o.io.out.ready) = Bool(true)
-      }
-      step(svars, ovars)
-      // bump counters and check outputs after advancing clock
-      if (ovars(o.io.in.ready).litValue() == 1 && (cycles % iDelay == 0) && (sourced < numOfInputs)) {
-        sourced += 1
-        sourcedIndex = sourced % inputData.length 
-        println("sourced and sourcedIndex are " + sourced + " " + sourcedIndex + " sinked is " + sinked)
-      }
-      if (ovars(o.io.out.valid).litValue() == 1) {
-        //When multi-threading or replication is used order of outputs are not preserved.
-        //Otherwise, we can check the values 
-        //allPassed = allPassed && (ovars(o.io.out.bits).litValue() == (inputData(sinkedIndex) + 2))
-        allPassed = allPassed && (ovars(o.io.out.bits.a).litValue() == (inputData(sinkedIndex) + 2))
-
-        if (allPassed == false) {
-			//println("Test failed because output is " + ovars(o.io.out.bits).litValue() + 
-			println("Test failed because output is " + ovars(o.io.out.bits.a).litValue() + 
-                          " expected " + (inputData(sinkedIndex) +2))
-			println("Sinked is " + sinked)
-        }
-		//println("At " + cycles + " output " + ovars(o.io.out.bits).litValue() + 
-		println("At " + cycles + " output " + ovars(o.io.out.bits.a).litValue() + 
-         " sinked. sinked is " + sinked);
-        sinked += 1
-        sinkedIndex = sinked % inputData.length 
-      }
-      cycles += 1
-    }
-    step(svars, ovars)
-
-    val UsePCReport = true 
-    if (!UsePCReport) {
-      // Inquire the back pressure through the PC ring
-      svars(o.io.pcIn.valid) = Bool(true)
-      svars(o.io.pcIn.bits.request) = Bool(true) 
-      svars(o.io.pcIn.bits.pcType) = Pcounters.backPressure
-      svars(o.io.pcIn.bits.moduleId) = UFix(3) //incthrough module Replicated
-      svars(o.io.pcIn.bits.portId) = UFix(1) //Input port
-      step(svars, ovars)
-      svars(o.io.pcIn.valid) = Bool(false)
-      while(ovars(o.io.pcOut.valid).litValue().intValue() == 0) {
-        step(svars, ovars)
-      }
-      println("PCREPORT: back pressure received " + ovars(o.io.pcOut.bits.pcValue))
-      println("cycles: " + cycles)
-    } else {
-      PCReport(cycles, numOfInputs)
-    }
-    step(svars, ovars)
-
-    allPassed 
+  //Spin for a while without any test input
+  for (time <- 0 until 5) {
+    poke(c.io.in.valid, false.B)
+    poke(c.io.in.bits.a, (0).U)
+    poke(c.io.pcIn.valid, false.B)
+    poke(c.io.pcIn.bits.pcType, Pcounters.pcReset)
+    poke(c.io.pcIn.bits.moduleId, (0).U)
+    poke(c.io.pcIn.bits.portId, (0).U)
+    step(1)
   }
+
+  //Reset the PC Ring
+  poke(c.io.pcIn.valid, true.B)
+  poke(c.io.pcIn.bits.request, true.B)
+  poke(c.io.pcIn.bits.pcType, Pcounters.pcReset)
+  step(1)
+  poke(c.io.pcIn.valid, false.B)
+  while(peek(c.io.pcOut.valid) == 0) {
+    step(1)
+  }
+  println("PCREPORT: Performance counter reset received")
+
+  //Actual tests
+  var sourced = 0
+  var sourcedIndex = 0
+  var sinked = 0
+  var sinkedIndex = 0
+  var allPassed = true
+  var cycles = 0
+  val numOfInputs = 10
+
+  while(sourced < numOfInputs && sinked < numOfInputs) {
+    if ((sourced < numOfInputs) && (cycles % iDelay == 0)) {
+      poke(c.io.in.bits.a, (inputData(sourcedIndex)).U)
+      poke(c.io.in.valid, true.B)
+      poke(c.io.out.ready, true.B)
+    } else {
+      poke(c.io.in.bits.a, (0).U)
+      poke(c.io.in.valid, false.B)
+      poke(c.io.out.ready, true.B)
+    }
+    if (peek(c.io.in.ready) == 1 && (cycles % iDelay == 0) && (sourced < numOfInputs)) {
+      sourced += 1
+      sourcedIndex = sourced % inputData.length
+      println("sourced and sourcedIndex are " + sourced + " " + sourcedIndex + " sinked is " + sinked)
+    }
+    if (peek(c.io.out.valid) == 1) {
+      //When multi-threading or replication is used order of outputs are not preserved.
+      //Otherwise, we can check the values
+      //allPassed = allPassed && (peek(c.io.out.bits) == (inputData(sinkedIndex) + 2))
+      allPassed = allPassed && (peek(c.io.out.bits.a) == (inputData(sinkedIndex) + 2))
+
+      if (allPassed == false) {
+        println("Test failed because output is " + peek(c.io.out.bits.a) +
+                " expected " + (inputData(sinkedIndex) + 2))
+        println("Sinked is " + sinked)
+      }
+        println("At " + cycles + " output " + peek(c.io.out.bits.a) +
+                " sinked. sinked is " + sinked)
+      sinked += 1
+      sinkedIndex = sinked % inputData.length
+    }
+    step(1)
+    cycles += 1
+  }
+  step(1)
+
+  val UsePCReport = true
+  if (!UsePCReport) {
+    // Inquire the back pressure through the PC ring
+    poke(c.io.pcIn.valid, true.B)
+    poke(c.io.pcIn.bits.request, true.B)
+    poke(c.io.pcIn.bits.pcType, Pcounters.backPressure)
+    poke(c.io.pcIn.bits.moduleId, (3).U) //incthrough module Replicated
+    poke(c.io.pcIn.bits.portId, (1).U) //Input port
+    step(1)
+    poke(c.io.pcIn.valid, false.B)
+    while(peek(c.io.pcOut.valid) == 0) {
+      step(1)
+    }
+    println("PCREPORT: back pressure received " + peek(c.io.pcOut.bits.pcValue))
+    println("cycles: " + cycles)
+  } else {
+    PCReport(cycles, numOfInputs)
+  }
+  step(1)
+
+  expect(allPassed.B, true.B)
 }
-
-
