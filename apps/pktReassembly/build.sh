bc8619800787044e47aca8174dcc4b7db62c9275
paleolithicman <768632866@qq.com>
2022-09-12 10:23:59 -0500

    update build script

--- apps/pktReassembly/build.sh
+++ apps/pktReassembly/build.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
 TARGET=pkt_reassembly_tail
-PRIMATE_DIR=/home/marui/crossroad/rv2
+PRIMATE_DIR=../../../
 LLVM_DIR=$PRIMATE_DIR/primate-arch-gen
-CHISEL_SRC_DIR=$PRIMATE_DIR/primate/chisel/Gorilla++/src
+UARCH_DIR=$PRIMATE_DIR/primate-uarch
+CHISEL_SRC_DIR=$UARCH_DIR/chisel/Gorilla++/src
 CUR_DIR=$(pwd)
-cd $PRIMATE_DIR/primate/compiler/engineCompiler/multiThread/
+cd $UARCH_DIR/compiler/engineCompiler/multiThread/
 make clean && make
-cd $PRIMATE_DIR/primate/apps/common/build/
+cd $UARCH_DIR/apps/common/build/
 make clean && make
 cd $CUR_DIR/sw
 $LLVM_DIR/build/bin/clang -emit-llvm -S -O3 "${TARGET}.cpp"
 $LLVM_DIR/build/bin/opt -enable-new-pm=0 -load $LLVM_DIR/build/lib/LLVMPrimate.so -primate < "${TARGET}.ll" > /dev/null
 mv primate.cfg $CHISEL_SRC_DIR/main/scala/
 mv header.scala $CHISEL_SRC_DIR/main/scala/
-cp input.txt $PRIMATE_DIR/primate/chisel/Gorilla++/
-mv primate_assembler.h $PRIMATE_DIR/primate/apps/scripts/
-cd $PRIMATE_DIR/primate/apps/scripts/
+cp input.txt $UARCH_DIR/chisel/Gorilla++/
+mv primate_assembler.h $UARCH_DIR/apps/scripts/
+cd $UARCH_DIR/apps/scripts/
 make clean && make
 cd $CUR_DIR/sw
-$PRIMATE_DIR/primate/apps/scripts/primate_assembler "${TARGET}.s" primate_pgm.bin
-mv primate_pgm.bin $PRIMATE_DIR/primate/chisel/Gorilla++/
+$UARCH_DIR/apps/scripts/primate_assembler "${TARGET}.s" primate_pgm.bin
+mv primate_pgm.bin $UARCH_DIR/chisel/Gorilla++/
 cd $CUR_DIR/hw
-cp $PRIMATE_DIR/primate/templates/primate.template ./
-python3 $PRIMATE_DIR/primate/apps/scripts/scm.py
+cp $UARCH_DIR/templates/primate.template ./
+python3 $UARCH_DIR/apps/scripts/scm.py
 cp *.scala $CHISEL_SRC_DIR/main/scala/
 [[ -e *.v ]] && cp *.v $CHISEL_SRC_DIR/main/resources/
 [[ -e *.sv ]] && cp *.sv $CHISEL_SRC_DIR/main/resources/
 rm primate.template
 rm primate.scala
-cd $PRIMATE_DIR/primate/templates
+cd $UARCH_DIR/templates
 cp *.scala $CHISEL_SRC_DIR/main/scala/
 cp *.v $CHISEL_SRC_DIR/main/resources/
 cd $CUR_DIR
