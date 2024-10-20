482948afb297d3e23bfac6451a6e49d322424920
Matthew Otto <106996253+Matthew-Otto@users.noreply.github.com>
2024-10-19 20:19:27 -0500

    add aes hw pipeline control

--- apps/aes/hw/aes_block_encrypt.sv
+++ apps/aes/hw/aes_block_encrypt.sv
@@ -2,6 +2,8 @@ module aes_block_encrypt #(parameter KEYLEN=128) (
     input  logic clk, rst,
     input  logic valid_in,
     output logic valid_out,
+    input  logic ready_out,
+    output logic ready_in,
     input  logic [KEYLEN/32+6:0][127:0] expanded_key,
     input  logic [3:0][3:0][7:0] plaintext,
     output logic [3:0][3:0][7:0] ciphertext
@@ -15,12 +17,15 @@ module aes_block_encrypt #(parameter KEYLEN=128) (
   logic [STD_ROUND_COUNT+1:0] ivalid;
   logic [STD_ROUND_COUNT+1:0][3:0][3:0][7:0] short_key;
   logic [STD_ROUND_COUNT:0][3:0][3:0][7:0] round_interm, ri_preflop;
+  
+  //TODO: skid buffer? when does ready_out deassert
+  assign ready_in = ready_out;
 
   // propagate a valid signal through the pipe
   assign ivalid[0] = valid_in;
   assign valid_out = ivalid[STD_ROUND_COUNT+1];
   for (i=0; i<STD_ROUND_COUNT+1; i++) begin : valid
-    flopr #(1) flop (clk, rst, ivalid[i], ivalid[i+1]);
+    flopr #(1) flop (clk, rst, .en(ready_out), ivalid[i], ivalid[i+1]);
   end
 
   // repartition keys
@@ -33,11 +38,11 @@ module aes_block_encrypt #(parameter KEYLEN=128) (
 
   for (i=0; i<STD_ROUND_COUNT; i++) begin : stdround
     aes_standard_round aesrnd (.key(short_key[i+1]), .state(round_interm[i]), .new_state(ri_preflop[i]));
-    flopr #(128) flop (clk, rst, ri_preflop[i], round_interm[i+1]);
+    flopr #(128) flop (clk, rst, .en(ready_out), ri_preflop[i], round_interm[i+1]);
   end
 
   aes_reduced_round aesrnd_short (.key(short_key[STD_ROUND_COUNT+1]),
     .state(round_interm[STD_ROUND_COUNT]), .new_state(ri_preflop[STD_ROUND_COUNT]));
-  flopr #(128) flop (clk, rst, ri_preflop[STD_ROUND_COUNT], ciphertext);
+  flopr #(128) flop (clk, rst, .en(ready_out), ri_preflop[STD_ROUND_COUNT], ciphertext);
 
 endmodule : aes_block_encrypt
---
+++ apps/aes/hw/floper.sv
@@ -0,0 +1,14 @@
+module flopr #(parameter WIDTH) (
+    input  logic clk, rst, en,
+    input  logic [WIDTH-1:0] d,
+    output logic [WIDTH-1:0] q
+);
+
+  always @(posedge clk) begin
+    if (rst)
+      q <= '0;
+    else if (en)
+      q <= d;
+  end
+
+endmodule : flopr
