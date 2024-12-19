c8e8101b64f2a30f522ee1dd4a5f3adfe51eeb78
Matthew-Otto <matthewotto@utexas.edu>
2024-12-19 14:43:42 -0600

    finish alu unit test

--- hw/templates/alu.scala
+++ hw/templates/alu.scala
@@ -12,67 +12,69 @@ class ALU(reg_width: Int) extends Module {
     val opcode    = Input(ALUOpCodes())
     val rs1       = Input(UInt(reg_width.W))
     val rs2       = Input(UInt(32.W))
-    val imm       = Input(UInt(32.W))
+    val imm       = Input(UInt(12.W)) // imm[11:0] (31:20)
     val immSel    = Input(Bool())
     val dout      = Output(UInt(reg_width.W))
   })
 
   val opcode_r = Reg(ALUOpCodes())
-  val opA_r    = Reg(SInt(32.W))
-  val opB_r    = Reg(SInt(32.W))
-  val result   = Wire(SInt(32.W))
+  val opA_r    = Reg(UInt(32.W))
+  val opB_r    = Reg(UInt(32.W))
+  val imm_r    = Reg(UInt(12.W))
+  val sr2      = Wire(UInt(32.W))
+  val result   = Wire(UInt(32.W))
 
   opcode_r  := io.opcode
-  opA_r     := io.rs1(31,0).asSInt
-  opB_r     := Mux(io.immSel, io.imm.asSInt, io.rs2(31,0).asSInt)
-  //val shamt = opB_r(4,0).asUInt
+  opA_r     := io.rs1(31,0)
+  opB_r     := io.rs2(31,0)
+  imm_r     := io.imm
+  sr2       := Mux(io.immSel, Fill(32-12, imm_r(11)) ## imm_r, opB_r)
   result    := DontCare
 
   switch (opcode_r) {
     is (add) {
-      result := opA_r + opB_r
+      result := opA_r + sr2
     }
     is (sub) {
-      result := opA_r - opB_r
+      result := opA_r - sr2
     }
     is (xor) {
-      result := opA_r ^ opB_r
+      result := opA_r ^ sr2
     }
     is (or) {
-      result := opA_r | opB_r
+      result := opA_r | sr2
     }
     is (and) {
-      result := opA_r & opB_r
+      result := opA_r & sr2
     }
     is (sll) {
-      result := opA_r << opB_r(4,0).asUInt
+      result := opA_r << sr2(4,0)
     }
     is (srl) {
-      result := (opA_r.asUInt >> opB_r(4,0).asUInt).asSInt
+      result := opA_r.asUInt >> sr2(4,0)
     }
     is (sra) {
-      result := opA_r >> opB_r(4,0).asUInt
+      result := (opA_r.asSInt >> sr2(4,0)).asUInt
     }
     is (slt) {
-      result := Mux(opA_r < opB_r, 1.U(32.W), 0.U).asSInt
+      result := Mux(opA_r.asSInt < sr2.asSInt, 1.U(32.W), 0.U) // todo sign extend sr2
     }
     is (sltu) {
-      result := Mux(opA_r.asUInt < opB_r.asUInt, 1.U(32.W), 0.U).asSInt
+      result := Mux(opA_r.asUInt < sr2, 1.U(32.W), 0.U)
     }
     is (lui) { // TODO: does this belong in ALU?
-      result := (opA_r(31,12) ## 0.U(12.W)).asSInt
+      result := (opA_r(31,12) ## 0.U(12.W))
     }
     is (cat) { // TODO: what is this for?
-      result := Cat(0.U(1.W),opB_r(8,0),opA_r(8,0)).asSInt
+      result := Cat(0.U(1.W),opB_r(8,0),opA_r(8,0))
     }
   }
 
   val rs1_r = Reg(UInt(reg_width.W))
   rs1_r := io.rs1
 
-  // why does the alu pass the upper bits of rs1 through to the output?
   if (reg_width > 32) {
-    io.dout := Cat(rs1_r(reg_width-1, 32), result.asUInt).asSInt
+    io.dout := Cat(rs1_r(reg_width-1, 32), result.asUInt)
   } else {
     io.dout := result.asUInt
   }
--- testing/basics/alu/alu_tb.py
+++ testing/basics/alu/alu_tb.py
@@ -35,16 +35,22 @@ async def test(dut):
     # TODO test immediate values
     for op in Opcode:
         print(f"testing opcode: {op}")
-        for _ in range(5):
-            a = random.getrandbits(32)
-            b = random.getrandbits(32)
-            imm = 0 # TODO rand
-            expected_val = function_map[op](a, b, imm)
 
-            actual_value = await evaluate_dut(dut, op.value, a, b, 0, 0)
-
-            # compare result
-            assert actual_value == expected_val, f"OP failed: {op} 0x{a:08x} 0x{b:08x} -> 0x{int(actual_value):08x} =/= 0x{expected_val:08x} (expected)"
+        print(f"testing predefine stimulus...", end="")
+        for imm in range(2):
+            for a,b in stimulus_map[op]:
+                expected_val = function_map[op](a, b, imm) & 0xFFFFFFFF
+                await evaluate_dut(dut, expected_val, op, a, b, b, imm)
+        print("pass")
+            
+        print(f"testing random stimulus...", end="")
+        for imm in range(2):
+            for _ in range(100):
+                a = random.getrandbits(32)
+                b = random.getrandbits(32)
+                expected_val = function_map[op](a, b, imm) & 0xFFFFFFFF
+                await evaluate_dut(dut, expected_val, op, a, b, b, imm)
+        print("pass")
 
 
 
@@ -62,16 +68,18 @@ async def reset_dut(dut):
     dut.reset.value = 0
 
 
-async def evaluate_dut(dut, opcode, rs1, rs2, imm, immSel):
+async def evaluate_dut(dut, expected_val, opcode, rs1, rs2, imm, immSel):
     """Drive DUT"""
-    dut.io_opcode.value = opcode
+    dut.io_opcode.value = opcode.value
     dut.io_rs1.value = rs1
     dut.io_rs2.value = rs2
-    dut.io_imm.value = imm
+    dut.io_imm.value = (imm & 0xFFF)
     dut.io_immSel.value = immSel
     await RisingEdge(dut.clock)
     await Timer(0.01, 'ns') # values don't update immediately after clock
-    return dut.io_dout.value
+    # compare result
+    print(f"dut.sr2: 0x{dut.sr2.value.to_unsigned():08x}")
+    assert dut.io_dout.value == expected_val, f"OP failed: {opcode} sr1: 0x{dut.io_rs1.value.to_unsigned():08x} sr2: 0x{dut.io_rs2.value.to_unsigned():08x} imm: 0x{dut.io_imm.value.to_unsigned():02x} immSel: {immSel} -> 0x{int(dut.io_dout.value):08x} =/= 0x{expected_val:08x} (expected)"
 
 
 def bits_to_signed_int(bits, bitlen):
@@ -82,57 +90,67 @@ def bits_to_signed_int(bits, bitlen):
         return bits
 
 
+def sign_ext(bits, bitlen):
+    """Takes an integer of <bitlen> length and sign extends it to 32 bit"""
+    bits &= (1 << bitlen) - 1
+    m = 1 << (bitlen - 1)
+    bits = (bits ^ m) - m
+    return bits & 0xFFFFFFFF
+
+
 def add(a, b, imm):
     if imm:
-        b = (b & 0x1F)
+        b = sign_ext(b, 12)
     result = a + b
     result &= 0xFFFFFFFF
     return result
 
 def sub(a, b, imm):
     if imm:
-        b = (b & 0x1F)
+        b = sign_ext(b, 12)
     result = a - b
     result &= 0xFFFFFFFF
     return result
 
 def xor(a, b, imm):
     if imm:
-        b = (b & 0x1F)
+        b = sign_ext(b, 12)
     return a ^ b
 
 def or_f(a, b, imm):
     if imm:
-        b = (b & 0x1F)
+        b = sign_ext(b, 12)
     return a | b
 
 def and_f(a, b, imm):
     if imm:
-        b = (b & 0x1F)
+        b = sign_ext(b, 12)
     return a & b
 
 def sll(a, b, _):
-    return (a << (b & 0x1f)) & 0xFFFFFFFF
+    return (a << (b & 0x1F)) & 0xFFFFFFFF
 
 def srl(a, b, _):
-    return (a >> (b & 0x1f)) & 0xFFFFFFFF
+    return (a >> (b & 0x1F)) & 0xFFFFFFFF
 
 def sra(a, b, _):
     if a & 0x80000000:
         a |= (0xFFFFFFFF << 32)
-    return (a >> (b & 0x1f)) & 0xFFFFFFFF
+    return (a >> (b & 0x1F)) & 0xFFFFFFFF
 
 def slt(a, b, imm):
-    a = bits_to_signed_int(a, 32)
     if imm:
-        b = bits_to_signed_int(b & 0x1F, 5)
-    else:
-        b = bits_to_signed_int(b, 32)
+        b = sign_ext(b, 12)
+    a = bits_to_signed_int(a, 32)
+    b = bits_to_signed_int(b, 32)
     return a < b
 
 def sltu(a, b, imm):
     if imm:
-        b = (b & 0x1F)
+        b = sign_ext(b, 12)
+    # convert to unsigned by masking to 32 bits
+    a &= 0xFFFFFFFF
+    b &= 0xFFFFFFFF
     return a < b
 
 def lui(a, _, __):
@@ -155,4 +173,19 @@ function_map = {
     Opcode.SLTU : sltu,
     Opcode.LUI : lui,
     Opcode.CAT : cat,
+}
+
+stimulus_map = {
+    Opcode.ADD : [(0,0),(1,0),(-1,0),(-1,1),((1<<31)-1,1),((1<<32)-1,1)],
+    Opcode.SUB : [(0,0),(1,0),(-1,0),(-1,1),((1<<31)-1,1),((1<<32)-1,1)],
+    Opcode.XOR : [(0,0),(1,0),(-1,0),(-1,1),(0xDEADBEEF,0xFFFFFFFF)],
+    Opcode.OR : [(0,0),(1,0),(-1,0),(-1,1),(0xDEADBEEF,0xFFFFFFFF)],
+    Opcode.AND : [(0,0),(1,0),(-1,0),(-1,1),(0xDEADBEEF,0xFFFFFFFF)],
+    Opcode.SLL : [(0xDEADBEEF,0),(0xDEADBEEF,1),(0xDEADBEEF,2),(0xDEADBEEF,10),(0xDEADBEEF,31)],
+    Opcode.SRL : [(0xDEADBEEF,0),(0xDEADBEEF,1),(0xDEADBEEF,2),(0xDEADBEEF,10),(0xDEADBEEF,31)],
+    Opcode.SRA : [(0xDEADBEEF,0),(0xDEADBEEF,1),(0xDEADBEEF,2),(0xDEADBEEF,10),(0xDEADBEEF,31)],
+    Opcode.SLT : [(0,0),(1,0),(0,1),(1,1),(-1,0),(-1,-1),(-1,1),(0,-1),(1,-1)],
+    Opcode.SLTU : [(0,0),(1,0),(0,1),(1,1),(-1,0),(-1,-1),(-1,1),(0,-1),(1,-1)],
+    Opcode.LUI : [(0xDEADBEEF,0)],
+    Opcode.CAT : [(0xDEAD,0xBEEF)],
 }
